7010a4a0cb15aa352fb4b1ca0f7f6aa8808c5b8b
Sharkbyte <38928133+cptretro@users.noreply.github.com>
2023-07-08 21:24:52 -0500

    added dear model and some state animation
    added dear model and some state animation

---
+++ Scripts/DeerAi.gd
@@ -0,0 +1,50 @@
+extends CharacterBody3D
+class_name DeerAi
+
+#signal deer_killed(position)
+
+@export var health := 10
+
+@export var state_manager : StateMachine
+@export var player: CharacterBody3D
+@onready var anim_tree = $deerhorse/AnimationTree
+
+func _ready():
+	# Not Implemented() :: Get signal from deer manager to look for nearby deer deaths
+	var test = get_tree().root.get_node("World")
+	print(test)
+	get_tree().root.get_node("World").get_node("DeerManager").deer_killed.connect(self.deer_is_killed)
+	
+	# Get Player reference
+	player = get_tree().root.get_node("World").get_node("Player")
+	
+func _physics_process(_delta):
+	move_and_slide()
+	
+	if velocity.length() > 0:
+		anim_tree.set("walk", 1)
+
+
+# Damage Script, clamps value to 0
+func take_damage(damage: float):
+	health = clamp(health - damage, 0, 10)
+	
+	if health == 0:
+		die()
+
+func deer_is_killed(pos: Vector3):
+	# throw NotImplementedException()
+	pass
+	# Check if deer is nearby && we've progressed to scary deer
+		# if nearby, tell state_manager to change state to eat_dead_bodies()
+		
+	#print("Signal heard!", pos)
+	
+	
+func die():
+	# throw new NotImplementedException()
+	pass
+
+
+func _on_deer_grazing_transitioned():
+	anim_tree.set("eat", 1)
